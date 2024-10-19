744617c7ccd728a4b9c9f98111644cecdf3b9b8f
xiejihuang@baidu.com <xiejihuang@baidu.com>
2024-10-19 19:39:15 +0800

    feat: 完成文本测试

--- src/pages/chat/App.tsx
+++ src/pages/chat/App.tsx
@@ -1,14 +1,78 @@
 import { useEffect, useState } from 'react'
 import { MenuMoreIcon, SendIcon } from '../Icon'
 export default () => {
-  const [message, setMessage] = useState<string[]>(['xxxx', '3233'])
+  const [status, setStatus] = useState<'open' | 'close'>('close')
+  const [socket, setSocket] = useState<WebSocket | null>(null)
+  const [message, setMessage] = useState<string[]>([])
   const [value, setValue] = useState('')
+  const map = {
+    open: '已连接',
+    close: '已断开'
+  }
+
+  const URI = 'ws://localhost:9601'
+
+  useEffect(() => {
+    // 创建 WebSocket 连接
+    const socket = new WebSocket(URI)
+    // 监听连接打开事件
+    socket.onopen = () => {
+      console.log('WebSocket 连接已建立')
+      setStatus('open')
+    }
+    // 监听消息事件
+    socket.onmessage = db => {
+      const event = JSON.parse(db.data)
+      console.log('event', event)
+      if (event.t == 'send_message') {
+        const txt = event.d.find((item: any) => item.t == 'text')
+        // 使用函数式更新
+        setMessage(prevMessages => prevMessages.concat([txt.d]))
+      }
+    }
+    // 监听连接关闭事件
+    socket.onclose = () => {
+      setStatus('close')
+      console.log('WebSocket 连接已关闭')
+    }
+    // 设置 socket 状态
+    setSocket(socket)
+    // 清理函数，关闭 WebSocket 连接
+    return () => {
+      socket.close()
+    }
+  }, [])
+
+  // 变动时自动清理
   useEffect(() => {
     setValue('')
   }, [message])
+
+  /**
+   *
+   * @param msg
+   */
+  const sendMessage = (msg: string) => {
+    if (socket && msg != '') {
+      socket.send(
+        JSON.stringify({
+          t: 'send_message',
+          d: [
+            {
+              t: 'text',
+              d: msg
+            }
+          ]
+        })
+      )
+    }
+  }
+
   return (
     <section className="bg-white h-full flex flex-col">
       <section className="h-6 flex  bg-slate-300 bg-opacity-80">
+        <div className="px-1">{URI}</div>
+        <div>{map[status]}</div>
         <div className="flex-1 drag-area"></div>
         <div className="px-2  cursor-pointer">
           <MenuMoreIcon />
@@ -29,13 +93,13 @@ export default () => {
           onChange={e => setValue(e.target.value)}
           placeholder="输入内容..."
           onKeyDown={event => {
-            event.key === 'Enter' && value != '' && setMessage(message.concat([value]))
+            if (event.key === 'Enter') sendMessage(value)
           }}
         />
         <div
           className="shadow-centent mx-2 cursor-pointer px-2 rounded-md  border-0"
           onClick={() => {
-            value != '' && setMessage(message.concat([value]))
+            sendMessage(value)
           }}
         >
           <SendIcon />
