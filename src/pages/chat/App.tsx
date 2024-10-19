82298588a300308165c235893e1635beb996d316
xiejihuang@baidu.com <xiejihuang@baidu.com>
2024-10-19 17:56:30 +0800

    feat: 增加键盘输入

--- src/pages/chat/App.tsx
+++ src/pages/chat/App.tsx
@@ -1,40 +1,37 @@
 import { useEffect, useState } from 'react'
 import { SendIcon } from '../Icon'
 export default () => {
-  const [message, setMessage] = useState<string[]>([])
+  const [message, setMessage] = useState<string[]>(['xxxx', '3233'])
   const [value, setValue] = useState('')
   useEffect(() => {
     setValue('')
+    // 滑动滚动条到下面
   }, [message])
   return (
     <section className="bg-white h-full flex flex-col">
-      <section className="h-6 drag-area bg-slate-300" />
-      <section className="flex-1 px-2 overflow-y-auto webkit">
+      <section className="h-6 drag-area bg-slate-300 bg-opacity-80" />
+      <section className="flex-1 px-3 py-2 overflow-y-auto flex gap-1 flex-col webkit bg-slate-50 bg-opacity-50">
         {message.map((item, index) => (
-          <div key={index}>{item}</div>
+          <div key={index} className=" bg-slate-200 bg-opacity-70 mr-auto p-1 rounded-md">
+            {item}
+          </div>
         ))}
       </section>
-      <section className="w-full flex flex-row justify-center p-1 bg-slate-400 ">
+      <section className="w-full flex flex-row justify-center p-1 bg-slate-400 bg-opacity-50">
         <input
           type="text"
-          className="rounded-md w-full h-8 px-2"
+          className="rounded-md w-full h-8 px-2 outline-none"
           value={value}
           onChange={e => setValue(e.target.value)}
           placeholder="输入内容..."
+          onKeyDown={event => {
+            if (event.key === 'Enter') {
+              setMessage(message.concat([value]))
+            }
+          }}
         />
         <div
-          className="
-            shadow-centent 
-            mx-2 
-            cursor-pointer 
-            px-2
-            rounded-md
-            border-0
-          hover:border-blue-500
-          focus:border-blue-500
-          focus:ring-blue-500
-            disabled:opacity-50 
-            disabled:pointer-events-none"
+          className="shadow-centent mx-2 cursor-pointer px-2 rounded-md  border-0"
           onClick={() => setMessage(message.concat([value]))}
         >
           <SendIcon />
