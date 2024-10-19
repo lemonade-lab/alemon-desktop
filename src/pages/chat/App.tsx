741cdd6108e04cea50d7a82b908294dcecc78c7f
xiejihuang@baidu.com <xiejihuang@baidu.com>
2024-10-19 20:25:05 +0800

    feat: 区分bot and user

--- src/pages/chat/App.tsx
+++ src/pages/chat/App.tsx
@@ -3,7 +3,12 @@ import { MenuMoreIcon, SendIcon } from '../Icon'
 export default () => {
   const [status, setStatus] = useState<'open' | 'close'>('close')
   const [socket, setSocket] = useState<WebSocket | null>(null)
-  const [message, setMessage] = useState<string[]>([])
+  const [message, setMessage] = useState<
+    {
+      bot: boolean
+      value: string
+    }[]
+  >([])
   const [value, setValue] = useState('')
   const map = {
     open: '已连接',
@@ -12,7 +17,11 @@ export default () => {
 
   const URI = 'ws://localhost:9601'
 
-  useEffect(() => {
+  /**
+   * 刷新状态
+   * @returns
+   */
+  const update = () => {
     // 创建 WebSocket 连接
     const socket = new WebSocket(URI)
     // 监听连接打开事件
@@ -27,7 +36,14 @@ export default () => {
       if (event.t == 'send_message') {
         const txt = event.d.find((item: any) => item.t == 'text')
         // 使用函数式更新
-        setMessage(prevMessages => prevMessages.concat([txt.d]))
+        setMessage(prevMessages =>
+          prevMessages.concat([
+            {
+              bot: true,
+              value: txt.d
+            }
+          ])
+        )
       }
     }
     // 监听连接关闭事件
@@ -35,6 +51,11 @@ export default () => {
       setStatus('close')
       console.log('WebSocket 连接已关闭')
     }
+    return socket
+  }
+
+  useEffect(() => {
+    const socket = update()
     // 设置 socket 状态
     setSocket(socket)
     // 清理函数，关闭 WebSocket 连接
@@ -54,6 +75,14 @@ export default () => {
    */
   const sendMessage = (msg: string) => {
     if (socket && msg != '') {
+      setMessage(prevMessages =>
+        prevMessages.concat([
+          {
+            bot: false,
+            value: msg
+          }
+        ])
+      )
       socket.send(
         JSON.stringify({
           t: 'send_message',
@@ -68,43 +97,66 @@ export default () => {
     }
   }
 
+  const [show, setShow] = useState(false)
+
+  const BOT_URI =
+    ' https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=300&h=300&q=80'
+
+  const USER_URI = 'https://q1.qlogo.cn/g?b=qq&s=0&nk=1715713618'
+
   return (
     <section className="bg-white h-full flex flex-col">
       <section className="h-6 flex  bg-slate-300 bg-opacity-80">
-        <div className="px-1">{URI}</div>
-        <div>{map[status]}</div>
-        <div className="flex-1 drag-area"></div>
-        <div className="px-2  cursor-pointer">
-          <MenuMoreIcon />
+        <div className="flex-1 drag-area text-center">
+          {URI} {map[status]}
+        </div>
+        <div className="px-2  cursor-pointer" onClick={() => setShow(!show)}>
+          {show ? <span className="text-sm">关闭</span> : <MenuMoreIcon />}
         </div>
       </section>
-      <section className="flex-1 px-3 py-2 overflow-y-auto flex gap-1 flex-col webkit bg-slate-50 bg-opacity-50">
-        {message.map((item, index) => (
-          <div key={index} className=" bg-slate-200 bg-opacity-70 mr-auto p-1 rounded-md">
-            {item}
-          </div>
-        ))}
-      </section>
-      <section className="w-full flex flex-row justify-center p-1 bg-slate-400 bg-opacity-50">
-        <input
-          type="text"
-          className="rounded-md w-full h-8 px-2 outline-none"
-          value={value}
-          onChange={e => setValue(e.target.value)}
-          placeholder="输入内容..."
-          onKeyDown={event => {
-            if (event.key === 'Enter') sendMessage(value)
-          }}
-        />
-        <div
-          className="shadow-centent mx-2 cursor-pointer px-2 rounded-md  border-0"
-          onClick={() => {
-            sendMessage(value)
-          }}
-        >
-          <SendIcon />
-        </div>
-      </section>
+      {!show && (
+        <>
+          <section className="flex-1 px-3 py-2 overflow-y-auto flex gap-1 flex-col webkit bg-slate-50 bg-opacity-50">
+            {message.map((item, index) => (
+              <div key={index} className=" flex  gap-4 bg-opacity-70 mr-auto ">
+                <img
+                  className="size-[3rem] rounded-full"
+                  src={item.bot ? BOT_URI : USER_URI}
+                  // src=""
+                  alt="Avatar"
+                />
+                <div className="rounded-md p-1 bg-slate-200">{item.value}</div>
+              </div>
+            ))}
+          </section>
+          <section className="w-full flex flex-row justify-center p-1 bg-slate-400 bg-opacity-50">
+            <input
+              type="text"
+              className="rounded-md w-full h-8 px-2 outline-none"
+              value={value}
+              onChange={e => setValue(e.target.value)}
+              placeholder="输入内容..."
+              onKeyDown={event => {
+                if (event.key === 'Enter') sendMessage(value)
+              }}
+            />
+            <div
+              className="shadow-centent mx-2 cursor-pointer px-2 rounded-md  border-0"
+              onClick={() => {
+                sendMessage(value)
+              }}
+            >
+              <SendIcon />
+            </div>
+          </section>
+        </>
+      )}
+      {show && (
+        <section className="flex-1 px-3 py-2 overflow-y-auto flex gap-1 flex-col webkit bg-slate-50 bg-opacity-50">
+          <div>用户名:text</div>
+          <div>平台</div>
+        </section>
+      )}
     </section>
   )
 }
